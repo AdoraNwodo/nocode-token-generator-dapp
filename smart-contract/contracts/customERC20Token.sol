71e7e0652043630e27140dd20d3f53d9867eff90
vuzzer <parfaitkouess@gmail.com>
2022-05-23 00:27:46 +0000

    add Ownable module of OpenZeppelin

--- smart-contract/contracts/customERC20Token.sol
+++ smart-contract/contracts/customERC20Token.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CustomERC20Token is ERC20 {
+contract CustomERC20Token is ERC20, Ownable {
     struct Token {
         string name;
         string symbol;
@@ -27,7 +28,7 @@ contract CustomERC20Token is ERC20 {
         token = _token;
         //initialSupply
         if (_token.isMintable) {
-            _mint(_msgSender(), _token.initialSupply);
+            ERC20._mint(_msgSender(), _token.initialSupply);
         }
     }
 
@@ -42,45 +43,37 @@ contract CustomERC20Token is ERC20 {
      *@dev Destroys `amount` tokens from `account`, reducing the
      * total supply
      */
-    function _burn(address _account, uint256 _amount)
-        internal
-        virtual
-        override
-    {
+    function burn(address _account, uint256 _amount) public onlyOwner {
         require(!token.isBurnable, "Token impossible to burn");
         require(_amount > 0, "No tokens");
         // burn `burnFeePercent` of amount
         if (token.hasFeesOrTaxes) {
             uint256 applyBurnFeePercentage = (_amount *
                 token.burnFeePercentage) / 100;
-            super._burn(_account, applyBurnFeePercentage);
+            ERC20._burn(_account, applyBurnFeePercentage);
         } else {
-            super._burn(_account, _amount);
+            ERC20._burn(_account, _amount);
         }
     }
 
-    function _transfer(
+    function transfer(
         address _from,
         address _to,
         uint256 _amount
-    ) internal virtual override {
+    ) public {
         if (token.hasFeesOrTaxes) {
             uint256 fees = (_amount * token.transactionTaxPercentage) / 100;
             uint256 amount = _amount - fees;
             //Transfer fees to Address must receive fees
-            super._transfer(_from, token.taxReceivingAddress, fees);
-            super._transfer(_from, _to, amount);
+            ERC20._transfer(_from, token.taxReceivingAddress, fees);
+            ERC20._transfer(_from, _to, amount);
         } else {
-            super._transfer(_from, _to, _amount);
+            ERC20._transfer(_from, _to, _amount);
         }
     }
 
-    /**
-     *@dev reward holder of token
-     *@param holder address of holder
-     */
-    function holderReward(address holder) internal {
-        require(token.doesHolderRedistribution, "No rewards for holders");
+    function holderReward(address holder) public onlyOwner {
+        require(token.doesHolderRedistribution, "No rewards for holder");
         transfer(holder, token.holdersRewardFee);
     }
 }
