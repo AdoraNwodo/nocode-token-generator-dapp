d8332c279dc9dd51fd7dc9f81ea8d3fc81b3e04c
vuzzer <parfaitkouess@gmail.com>
2022-05-27 20:36:24 +0000

    add mint function in constructor of ERC20Token.sol

--- smart-contract/contracts/CustomERC20.sol
+++ smart-contract/contracts/CustomERC20.sol
@@ -3,14 +3,9 @@
 pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CustomERC20 is Ownable {
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    event Transfer(address _from, address _to, uint256 _amount);
-    event Approval(address _owner, address _spender, uint _amount);
-
+contract CustomERC20 is ERC20, Ownable {
     // Token properties
     string private _name;
     string private _symbol;
@@ -18,110 +13,29 @@ contract CustomERC20 is Ownable {
     bool private _isMintable;
     uint256 private _totalSupply;
 
-    constructor(string memory name_, string memory symbol_, bool isBurnable_, bool isMintable_, uint256 initialSupply_) {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        bool isBurnable_,
+        bool isMintable_,
+        uint256 initialSupply_
+    ) ERC20(name_, symbol_) {
         _name = name_;
         _symbol = symbol_;
         _isBurnable = isBurnable_;
         _isMintable = isMintable_;
         _totalSupply = initialSupply_;
+        ERC20._mint(tx.origin, initialSupply_ * 10**18);
         transferOwnership(tx.origin);
     }
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address to, uint256 amount) public returns (bool) {
-        address owner = msg.sender;
-        _transfer(owner, to, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        address owner = msg.sender;
-        _approve(owner, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        address spender = msg.sender;
-        _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function _transfer(address from, address to, uint256 amount) internal {
-        require(from != address(0), "transfer from the zero address");
-        require(to != address(0), "transfer to the zero address");
-
-        uint256 fromBalance = _balances[from];
-        require(fromBalance >= amount, "transfer amount exceeds balance");
-        unchecked {
-            _balances[from] = fromBalance - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(from, to, amount);
-    }
-
-    function _mint(address account, uint256 amount) internal {
+    function mint(address account, uint256 amount) public onlyOwner {
         require(_isMintable, "token is not mintable");
-        require(account != address(0), "mint to the zero address");
-
-        _totalSupply += amount;
-        _balances[account] += amount;
-        emit Transfer(address(0), account, amount);
+        ERC20._mint(account, amount);
     }
 
-    function _burn(address account, uint256 amount) internal {
+    function burn(address account, uint256 amount) public {
         require(_isBurnable, "token cannot be burned");
-        require(account != address(0), "burn from the zero address");
-
-        uint256 accountBalance = _balances[account];
-        require(accountBalance >= amount, "burn amount exceeds balance");
-        unchecked {
-            _balances[account] = accountBalance - amount;
-        }
-        _totalSupply -= amount;
-
-        emit Transfer(account, address(0), amount);
-    }
-
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "approve from the zero address");
-        require(spender != address(0), "approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _spendAllowance(address owner, address spender, uint256 amount) internal {
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            require(currentAllowance >= amount, "insufficient allowance");
-            unchecked {
-                _approve(owner, spender, currentAllowance - amount);
-            }
-        }
+        ERC20._burn(account, amount);
     }
 }
